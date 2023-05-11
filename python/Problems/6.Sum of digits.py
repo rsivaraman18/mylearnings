3969bcc6c319fc91d7f56e1489cacd070d74421d
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2023-05-11 10:14:30 +0530

    Add files via upload

---
+++ python/Problems/6.Sum of digits.py
@@ -0,0 +1,9 @@
+def sumofdigits():
+    number = input("Enter any number : ")
+    temp = str(number)
+    total = 0
+    for i in range(0,len(temp)):
+        total = total + int(temp[i])
+    print(total)
+
+sumofdigits() 

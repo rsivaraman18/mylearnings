aa5f5fd51fb04379997bc68b007af68c8167d452
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2023-05-11 10:15:13 +0530

    Add files via upload

---
+++ python/Problems/7.Reverse of a given Number.py
@@ -0,0 +1,12 @@
+
+def reverse():
+    n = int(input("Enter any number to reverse : "))
+    rev = 0
+    while( n>0):
+        dig=n%10
+        
+        rev=rev*10 + dig
+        n=n//10
+    print('REVERSE OF A NUMBER:',rev )
+
+reverse()

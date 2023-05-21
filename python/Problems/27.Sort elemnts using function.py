07b6535f07742156625925e2b392e47811bda37f
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2023-05-21 08:47:05 +0530

    Add files via upload

---
+++ python/Problems/27.Sort elemnts using function.py
@@ -0,0 +1,14 @@
+# ()
+def sort():
+           
+    l = [25,10,15,55,35,45,5,75,65,85,90,70,100]
+    print('given list -->',l )
+    l.sort()
+
+    print('Sort Ascend -->',l )
+    l.sort(reverse = True)
+
+    print('Sort Descend -->',l )
+
+
+sort()  

45f7805278cfb078a1ad7297ebd1afb6a14bc85a
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2023-05-22 08:25:35 +0530

    Add files via upload

---
+++ python/Problems/30.Print Number Of words.py
@@ -0,0 +1,10 @@
+sentence = 'I am an Engineer.I want to work.I am ready to work.'
+
+words  = len(sentence.split())
+letter = len(sentence)
+sent   = len(sentence.split('.'))-1
+
+print('No of words in given sentence is' , words)
+print('No of letters in given sentence is' , letter)
+print('No of sentence in a given paragraph is' , sent)
+

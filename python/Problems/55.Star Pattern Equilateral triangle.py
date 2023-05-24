228ba5eccc4a33aca1b5f54d0d5c2b523cb3583a
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2023-05-24 12:03:00 +0530

    Update 55.Star Pattern Equilateral triangle.py

--- python/Problems/55.Star Pattern Equilateral triangle.py
+++ python/Problems/55.Star Pattern Equilateral triangle.py
@@ -1,7 +1,7 @@
 def triangle1(n):
     print('INverted triangle')
     for row in range(1,n+1):
-        #print(' '*(row-1) , ' *'*(n-row))
+        #print(' '*(row-1) , ' *'*(n-row)) 
         print(' '*(row) , ' *'*(n-row+1))
          
 

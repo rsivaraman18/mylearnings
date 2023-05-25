5f53d338e1998c217c05af192f13eee23247c41f
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2023-05-25 12:25:58 +0530

    Add files via upload

---
+++ python/Problems/57.Number pattern -Square.py
@@ -0,0 +1,34 @@
+# ()
+
+def square1(n):
+    for row in range(1,n+1):
+        for col in range(1,n+1):
+            print(col,end='')
+        print()
+    print('******************')   
+
+
+
+def square2(n):
+    for row in range(1,n+1):
+        for col in range(1,n+1):
+            print(row,end='')
+        print()
+    print('******************')   
+
+
+def square3(n):
+    for row in range(n,0,-1):
+        for col in range(1,n+1):
+            print(row,end='')
+        print()
+    print('******************')   
+
+
+
+
+
+
+square1(5)       
+square2(5)
+square3(7)

4629c8607693b9d696f932d493fbec6474c784f2
rsivaraman18 <126255552+rsivaraman18@users.noreply.github.com>
2023-05-31 09:31:02 +0530

    Update 56.Star Pattern Diamond.py

--- python/Problems/56.Star Pattern Diamond.py
+++ python/Problems/56.Star Pattern Diamond.py
@@ -3,7 +3,7 @@ def diamond(n):
     for row in range(1,n+1):
         print(' '*(n-row+1) , ' *'*(row))   # Extra space
     for row in range(1,n+1):
-        print(' '*(row+1) , ' *'*(n-row))   # Extra space
+        print(' '*(row+1) , ' *'*(n-row))   #  Extra space
         
 
 diamond(7)
